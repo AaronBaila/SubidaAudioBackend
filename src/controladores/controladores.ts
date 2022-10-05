61475bbee67a5f1267cfded4b0d9e970925571b8
a_aaaa <a@a.com>
2022-10-05 17:50:37 +0200

    completar_rutas

--- src/controladores/controladores.ts
+++ src/controladores/controladores.ts
@@ -1,24 +1,22 @@
 import {Request, Response} from 'express';
 
 
-function getTodo(req: Request, res: Response): Response {
+export  function getTodo(req: Request, res: Response): Response {
     return res.send('HelloWord');
 }
 
-function getUno(req: Request, res: Response): Response {
+export function getUno(req: Request, res: Response): Response {
     return res.send('HelloWord');
 }
 
-function publicar(req: Request, res: Response): Response {
+export  function publicar(req: Request, res: Response): Response {
     return res.send('HelloWord');
 }
 
-function modificar(req: Request, res: Response): Response {
+export function modificar(req: Request, res: Response): Response {
     return res.send('HelloWord');
 }
 
-function eliminar(req: Request, res: Response): Response {
+export function eliminar(req: Request, res: Response): Response {
     return res.send('HelloWord');
 }
-
-export * from './controladores';
--- src/database.ts
+++ src/database.ts
@@ -3,5 +3,5 @@ import mongoose from 'mongoose';
 export function conexionBBDD(){
     mongoose.connect('mongodb://127.0.0.1/baseDatos')
         .then(db => console.log("Conectado a la BBDD"))
-        .catch(err => console.log("Error al conectarse a la BBDD" + err));
+        .catch(err => console.log("Error al conectarse a la BBDD: " +  "  " + err));
 };
